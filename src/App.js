a54da40c6e6d73075ad472e460110fc2d9ec403e
Jeff Bloom <bloo0178@gmail.com>
2019-04-06 18:42:18 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import background from './images/Group.svg';
+import styles from './App.module.css';
+import {Component1} from './components/Component1';
+import {Component2} from './components/Component2';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={styles.Container}>
+        <Component1 />
+        <Component2 />
       </div>
     );
   }
